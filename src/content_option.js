2e73196410f60e1c0a28ae0915514a99d62b2d0c
Bui Trung Hieu <buitrunghieu@Hieus-MacBook-Pro.local>
2022-08-04 18:22:49 +1000

    2nd

---
+++ src/content_option.js
@@ -0,0 +1,170 @@
+const logotext = "DERRICK";
+const meta = {
+    title: "Derrick Bui",
+    description: "I’m Derrick Bui, I'm currently studying in Australia, my major is Software Developer, and I'm a full stack web developer.",
+};
+
+const introdata = {
+    title: "I’m Derrick Bui",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "My name is Derrick Bui, I'm 20 years old and currently studying in Australia, my major is Software Development and Marketing",
+    your_img_url: "https://scontent.fsyd11-1.fna.fbcdn.net/v/t39.30808-6/269761861_1121234178674828_1564516782693387576_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=e3f864&_nc_ohc=_UydBB_5Pw4AX8u181j&_nc_ht=scontent.fsyd11-1.fna&oh=00_AT93s9l2Zc_0K-iW5ynCPlYXs7xAzqk6W-8HsCBEhGYxXg&oe=62EFF9E2",
+};
+
+const dataabout = {
+    title: "A bit about myself",
+    aboutme: "Adventurous and Aspiring Software Developer and Digital Marketing | Seeking Entry-level Full Stack Developing/Programming/Marketing Internships and Co-op Opportunities |",
+};
+const worktimeline = [{
+        jobtitle: "English Teaching Assistant",
+        where: "ECE - Hanoi",
+        date: "Aug 2020 - Oct 2021",
+    },
+    {
+        jobtitle: "Full Stack Developer Intern",
+        where: "Benit.io - Hanoi",
+        date: "Oct 2021 - Dec 2021",
+    },
+    {
+        jobtitle: "Vietnamese Casual Market Interviewer",
+        where: "McNair YellowSquares - Sydney",
+        date: "July 2022 - Now",
+    },
+];
+
+const skills = [{
+        name: "Python",
+        value: 80,
+    },
+    {
+        name: "Java",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "C++",
+        value: 60,
+    },
+    {
+        name: "Swift",
+        value: 70,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Web Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "(555)123-4567",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+    youtube: "https://youtube.com",
+    twitch: "https://twitch.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
